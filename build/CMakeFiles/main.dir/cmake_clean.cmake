db82988270f75b87b0eff305f5f966c42d0471cc
kangjie2333 <936874728@qq.com>
2022-04-06 21:49:38 +0800

    重写cmakelists.txt

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/libmain.dll.a"
+  "../bin/main.exe"
+  "../bin/main.exe.manifest"
+  "../bin/main.pdb"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "CMakeFiles/main.dir/src/test.cpp.obj"
+  "CMakeFiles/main.dir/src/test.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
